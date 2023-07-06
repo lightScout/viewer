c219b100ee5bd1ba82c0c55be5ddb2e02d09332f
lightScout <juanmullercs@hotmail.com>
2023-07-06 12:22:18 +0200

    add Constants.kt

---
+++ app/src/main/java/com/sample/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.sample.util
+
+class Constants {
+    companion object{
+        const val BASE_URL = "http://api.duckduckgo.com"
+        const val QUERY = "q"
+        const val FORMAT = "format"
+
+    }
+}
