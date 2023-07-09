b22f818a721757f09b05f5555a337cfd8e2045ae
lightScout <juanmullercs@hotmail.com>
2023-07-09 17:12:15 +0200

    add CharacterListAdapter.kt

---
+++ app/src/main/java/com/sample/view/adapter/CharacterListAdapter.kt
@@ -0,0 +1,40 @@
+package com.sample.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.RecyclerView
+import com.sample.databinding.CharacterItemAdapterLayoutBinding
+import com.sample.model.data.RelatedTopic
+
+class CharacterListAdapter(private var charactersList: MutableList<RelatedTopic>) :
+    RecyclerView.Adapter<CharacterListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: CharacterItemAdapterLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CharacterItemAdapterLayoutBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.animation =
+            AnimationUtils.loadAnimation(holder.itemView.context, android.R.anim.slide_in_left)
+        charactersList[position].let {
+            holder.binding.characterNameTextView.text = it.Text
+        }
+    }
+
+    override fun getItemCount(): Int = charactersList.size
+
+    fun updateList(charactersList: List<RelatedTopic>) {
+        this.charactersList.clear()
+        this.charactersList.addAll(charactersList)
+        notifyDataSetChanged()
+    }
+}
