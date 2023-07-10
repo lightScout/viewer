77cedc36894f56e00af4047e5cab03f23d5fd2a6
lightScout <juanmullercs@hotmail.com>
2023-07-10 12:25:02 +0200

    implement CharacterListFragment.kt

--- app/src/main/java/com/sample/view/activity/MainActivity.kt
+++ app/src/main/java/com/sample/view/activity/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.sample.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sample.R
-import com.sample.viewmodel.CharactersListViewModel
-import com.sample.viewmodel.CharactersListViewModelState
-import org.koin.android.ext.android.inject
+import com.sample.databinding.ActivityMainBinding
+import com.sample.view.fragment.CharacterListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: CharactersListViewModel by inject()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel.charactersListState.observeForever { state ->
-            when (state) {
-                is CharactersListViewModelState.Loading -> {}
-                else -> {}
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        launchCharacterListFragment()
     }
+
+    private fun launchCharacterListFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_1, CharacterListFragment())
+            .commit()
+    }
+
+
 }
---
+++ app/src/main/java/com/sample/view/fragment/CharacterListFragment.kt
@@ -0,0 +1,67 @@
+package com.sample.view.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.sample.databinding.CharacterListFragmentLayoutBinding
+import com.sample.view.adapter.CharacterListAdapter
+import com.sample.viewmodel.CharactersListViewModel
+import com.sample.viewmodel.CharactersListViewModelState
+import org.koin.android.ext.android.inject
+
+
+class CharacterListFragment : Fragment() {
+
+    private lateinit var binding: CharacterListFragmentLayoutBinding
+    private val viewModel: CharactersListViewModel by inject()
+    private val listAdapter = CharacterListAdapter(mutableListOf())
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = CharacterListFragmentLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUI()
+        observeViewModel()
+    }
+
+    private fun setupUI() {
+        binding.let {
+            it.characterListRecyclerView.addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            it.characterListRecyclerView.layoutManager =
+                androidx.recyclerview.widget.LinearLayoutManager(context)
+            it.characterListRecyclerView.adapter = listAdapter
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.charactersListState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is CharactersListViewModelState.Loading -> {
+                    binding.characterListProgressBar.visibility = View.VISIBLE
+                }
+                is CharactersListViewModelState.ShowListOfCharacters -> {
+                    binding.characterListProgressBar.visibility = View.GONE
+                    listAdapter.updateList(state.characters)
+                }
+                else -> {
+                    // not required
+                }
+            }
+        }
+    }
+}
